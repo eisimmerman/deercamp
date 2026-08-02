51ceb10b1fade66198a504cb21cae6deec618a1b
Eric Simmerman <simmerman.eric@gmail.com>
2026-08-02 02:05:41 -0500

    Lock completed Camp Boddington Archives Room demo

---
+++ presentations/craig-boddington/archives-v7-build/archive-room-config.js
@@ -0,0 +1,26 @@
+window.ARCHIVE_ROOM_CONFIG={
+  routes:{
+    back:'../../../../camp-boddington-wired-test/index.html?view=tabletop',
+    archives:'../archive-room/index.html',
+    maps:'../maps-room/index.html',
+    campfire:'../campfire-room/',
+    memory:'../memory-voice-room/'
+  },
+  actions:{
+    legacySeries:{title:'Legacy Series',url:''},
+    campTimeline:{title:'Camp Timeline',url:''},
+    campFounders:{title:'Camp Founders',url:''},
+    historicPhotos:{title:'Historic Photos',url:''},
+    historicArtifacts:{title:'Historic Artifacts',url:''},
+    campRecords:{title:'Camp Records',url:''},
+    pressPublications:{title:'Press & Publications',url:''},
+    searchArchives:{title:'Search Archives',url:''},
+    addHistoricPhoto:{title:'Add Historic Photo',url:''},
+    scanDocument:{title:'Scan Document',url:''},
+    uploadArtifact:{title:'Upload Artifact',url:''},
+    writeCampHistory:{title:'Write Camp History',url:''},
+    addTimelineEvent:{title:'Add Timeline Event',url:''},
+    createLegacyCollection:{title:'Create Legacy Collection',url:''},
+    saveToArchives:{title:'Save to Archives',url:''}
+  }
+};
